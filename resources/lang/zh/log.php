b362ff70d5b23da45c10dbc77389bff65fe6327f
lizhicai <lizhicai@rybbaby.com>
2016-09-23 17:11:56 +0800

    first commit

---
+++ resources/lang/zh/log.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'new_permission' => '创建了新的权限 :name。',
+    'updated_permission' => '更新权限 :name。',
+    'deleted_permission' => '删除权限 :name。',
+
+    'new_role' => '创建新角色 :name。',
+    'updated_role' => '更新角色 :name。',
+    'deleted_role' => '删除角色 :name。',
+    'updated_role_permissions' => '更新角色权限。',
+
+    'logged_in' => '登录。',
+    'logged_out' => '退出。',
+    'created_account' => '创建一个帐户。',
+    'updated_avatar' => '更新个人信息头像。',
+    'updated_profile' => '更新个人信息。',
+    'deleted_user' => '删除用户 :name.',
+    'banned_user' => '禁止用户 :name.',
+    'updated_profile_details_for' => '更新个人信息详情 :name.',
+    'created_account_for' => '为用户创建一个帐户 :name.',
+    'updated_settings' => '更新网站设置.',
+    'enabled_2fa' => 'Enabled Two-Factor Authentication.',
+    'disabled_2fa' => 'Disabled Two-Factor Authentication.',
+    'enabled_2fa_for' => 'Enabled Two-Factor Authentication for user :name.',
+    'disabled_2fa_for' => 'Disabled Two-Factor Authentication for user :name.',
+    'requested_password_reset' => '请求密码重置电子邮件。',
+    'reseted_password' => '重设密码使用“忘记密码”选项。'
+];
---
+++ resources/lang/zh/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码必须至少为6个字符，并匹配确认。',
+    'reset' => '您的密码已被重置！',
+    'sent' => '我们给你的密码重置链接！',
+    'token' => '此密码重置令牌无效。',
+    'user' => "我们找不到那个电子邮件地址的用户。",
+
+];
